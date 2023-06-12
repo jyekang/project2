dd0c44a3a5e53873ba9684b0b8854d7f779d9af5
jyekang <janicekangaroo@gmail.com>
2023-06-12 07:55:46 -0700

    created folders and server

---
+++ server.js
@@ -0,0 +1,19 @@
+// require('dotenv').config();
+
+const express = require('express')
+const PORT = process.env.PORT || 3002
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(PORT, () => {
+    console.log(`Express server listening on port ${PORT}`)
+  })
+
+  app.get('/', (req, res) => {
+    res.send('server working')
+  })
