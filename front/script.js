1d743848cdcfe1fd1f1d78fccbcc122d52123baa
jyekang <janicekangaroo@gmail.com>
2023-06-13 11:50:09 -0700

    connected get all photos to gallery

--- front/script.js
+++ front/script.js
@@ -1,1 +1,21 @@
-const BASE_URL = `http://localhost:3004`
+console.log('working')
+
+const getPhotos = async () => {
+    const response = await axios.get(`http://localhost:3002/api/getallphotos`)
+    let data = response.data
+    console.log(data)
+    const GI1 = document.querySelector("#GI1")
+    GI1.src = '/images/' + response.data.photos[0].image
+  }
+
+
+getPhotos()
+// const photos = getPhotos()
+// console.log(photos)
+// const response = await axios.get(`http://localhost:3002/api/getallphotos`)
+// console.log(response)
+
+
+
+// GI1.src = response.data.photos[0].image
+ 
