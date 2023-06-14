3aeaf1450a1868535627687504c0652a2f27a50c
jyekang <janicekangaroo@gmail.com>
2023-06-14 09:20:39 -0700

    added album.js

---
+++ models/album.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Album = new Schema (
+    {
+        title: { type: String, required: true},
+        image: [Photo],
+        datecreated: { type: Date, required: true}
+    },
+    { timestamps: true }
+)
+
+
+module.exports = mongoose.model('album', Album)
