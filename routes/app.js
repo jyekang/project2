067c0da9d057f628cc0a92f6dddbecd5f088b57f
jyekang <janicekangaroo@gmail.com>
2023-06-15 06:39:58 -0700

    added client router

--- routes/app.js
+++ routes/app.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const albumrouter = require('./album')
 const photorouter = require('./main')
+const clientrouter = require('./client')
 
 router.use('/', photorouter)
 router.use('/', albumrouter)
+router.use('/', clientrouter)
 
 module.exports = router
