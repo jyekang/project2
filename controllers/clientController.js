3639e15ebd46462125ac1c685b69f2d4f0140fa1
jyekang <janicekangaroo@gmail.com>
2023-06-14 20:32:20 -0700

    inserted client controller

---
+++ controllers/clientController.js
@@ -0,0 +1,14 @@
+const Client = require('../models/client')
+
+const getAllClients = async (req, res) => {
+    try {
+        const clients = await Client.find()
+        return res.status(200).json({ clients })
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+module.exports = {
+    getAllClients
+}
